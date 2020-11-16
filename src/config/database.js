e4941de102a3f87a1fc2c4f771a2fcad158f7d89
Matheus Melo <groove.street.x@hotmail.com>
2020-11-16 11:52:41 -0300

    Connection with local database added

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+// Connection with PostgreSQL Database
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
